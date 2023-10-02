13f887bcebfb5ff223facdfe190a622c0dd8b909
Jorge Antônio <90734777+jorgespark11@users.noreply.github.com>
2023-10-01 22:28:10 -0300

    Update page.php

--- ENGENHARIADESOFTWARE2023.2/resources/View/Pages/page.php
+++ ENGENHARIADESOFTWARE2023.2/resources/View/Pages/page.php
@@ -104,51 +104,38 @@ $decoded = JWT::decode($token, new Key($chaveSecreta, 'HS256'));
         </div>
     </main>
 
-    <footer class="container py-5">
-        <div class="row">
-            <div class="col-12 col-md">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block mb-2">
-                    <circle cx="12" cy="12" r="10"></circle>
-                    <line x1="14.31" y1="8" x2="20.05" y2="17.94"></line>
-                    <line x1="9.69" y1="8" x2="21.17" y2="8"></line>
-                    <line x1="7.38" y1="12" x2="13.12" y2="2.06"></line>
-                    <line x1="9.69" y1="16" x2="3.95" y2="6.06"></line>
-                    <line x1="14.31" y1="16" x2="2.83" y2="16"></line>
-                    <line x1="16.62" y1="12" x2="10.88" y2="21.94"></line>
-                </svg>
-                <small class="d-block mb-3 text-muted">© 2023</small>
-            </div>
-            <div class="col-6 col-md">
-                <h5>Recursos</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Recursos legais</a></li>
-                    <li><a class="text-muted" href="#">Recurso aleatório</a></li>
-                    <li><a class="text-muted" href="#">Recurso da equipe</a></li>
-                    <li><a class="text-muted" href="#">Coisas para desenvolvedores</a></li>
-                    <li><a class="text-muted" href="#">Outro</a></li>
-                    <li><a class="text-muted" href="#">Última vez</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>Recursos</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Recurso</a></li>
-                    <li><a class="text-muted" href="#">Nome do recurso</a></li>
-                    <li><a class="text-muted" href="#">Outro recurso</a></li>
-                    <li><a class="text-muted" href="#">Recurso final</a></li>
-                </ul>
-            </div>
-            <div class="col-6 col-md">
-                <h5>Sobre</h5>
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Equipe</a></li>
-                    <li><a class="text-muted" href="#">Localizações</a></li>
-                    <li><a class="text-muted" href="#">Privacidade</a></li>
-                    <li><a class="text-muted" href="#">Termos</a></li>
-                </ul>
-            </div>
+  
+<footer class="container py-5">
+    <div class="row">
+
+        <div class="col-6 col-md">
+            <h5>Contato</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Numero</a></li>
+                <li><a class="text-muted" href="#">Email</a></li>
+                <li><a class="text-muted" href="#">Instagram</a></li>
+                <li><a class="text-muted" href="#">Etc</a></li>
+            </ul>
         </div>
-    </footer>
+        <div class="col-6 col-md">
+            <h5>Ajuda</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Forum</a></li>
+                <li><a class="text-muted" href="#">Atendimento</a></li>
+                <li><a class="text-muted" href="#">Etc</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <h5>Sobre</h5>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="#">Equipe</a></li>
+                <li><a class="text-muted" href="#">Localizações</a></li>
+                <li><a class="text-muted" href="#">Privacidade</a></li>
+                <li><a class="text-muted" href="#">Termos</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
 
 
 </body>
