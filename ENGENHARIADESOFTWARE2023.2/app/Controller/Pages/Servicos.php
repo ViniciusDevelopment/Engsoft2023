6248f6563b5f0aee05c035ebb2898f7b2a420f09
PC Jorge <90734777+jorgespark11@users.noreply.github.com>
2023-10-03 08:20:33 -0300

    Função alterar Serviços

--- ENGENHARIADESOFTWARE2023.2/app/Controller/Pages/Servicos.php
+++ ENGENHARIADESOFTWARE2023.2/app/Controller/Pages/Servicos.php
@@ -14,7 +14,7 @@ class Servico
     {
         $conexao = new Conexao;
         $conectado = $conexao->conectarBancoDeDados();
-        
+
         $sql = "INSERT INTO servicos (Nome, Valor, Descricao, disponibilidade, prestador_id) VALUES (?, ?, ?, 1, ?)";
         $stmt = $conectado->prepare($sql);
         $stmt->bind_param("sdsi", $nome, $valor, $descricao, $id_prestador);
@@ -33,5 +33,41 @@ class Servico
         $result = $conectado->query($sql);
         return $result;
     }
+<<<<<<< Updated upstream
     
+=======
+    public static function ConsultarServicosPrestador($id_prestador)
+    {
+        $conexao = new Conexao;
+        $conectado = $conexao->conectarBancoDeDados();
+        $sql = "SELECT * FROM servicos WHERE prestador_id = $id_prestador";
+        $result = $conectado->query($sql);
+        return $result;
+    }
+
+    public static function ConsultarServicosid($id)
+    {
+        $conexao = new Conexao;
+        $conectado = $conexao->conectarBancoDeDados();
+        $sql = "SELECT * FROM servicos WHERE id = $id";
+        $result = $conectado->query($sql);
+        return $result;
+    }
+
+    public static function alterarservico($id, $nome, $valor, $descricao, $disponibilidade)
+    {
+      $conexao = new Conexao;
+      $conectado = $conexao->conectarBancoDeDados();
+
+      $sql = "UPDATE servicos SET Nome = ?, Valor = ?, Descricao = ?, disponibilidade = ? WHERE id = ?";
+      $stmt = $conectado->prepare($sql);
+      $stmt->bind_param("sdssi", $nome, $valor, $descricao, $disponibilidade, $id);
+
+      if ($stmt->execute()) {
+          return true; // Atualização bem-sucedida
+      } else {
+          return false; // Erro na atualização
+      }
+  }
+>>>>>>> Stashed changes
 }
--- ENGENHARIADESOFTWARE2023.2/resources/View/Pages/CadastrarServiço.php
+++ ENGENHARIADESOFTWARE2023.2/resources/View/Pages/CadastrarServiço.php
@@ -2,6 +2,37 @@
 require_once '..\..\..\app\Controller\Pages\Servicos.php';
 use \App\Controller\Pages\Servico;
 $Servico = new Servico;
+<<<<<<< Updated upstream
+=======
+
+if(isset($_POST['nomeServico']))
+{
+    $nome = $_POST['nomeServico'];
+    $valor = $_POST['valorServico'];
+    $descricao = $_POST['descricaoServico'];
+    $id_prestador = $decoded->id;
+
+    $retornoCadastroServico = $Servico->CadastrarServiço($nome, $valor, $descricao,$id_prestador);
+
+    echo '<div class="d-flex justify-content-center mt-3">';
+
+    if ($retornoCadastroServico) {
+        // Redireciona para a mesma página com um parâmetro "success"
+        header('Location: page.php?arquivo=CadastrarServiço&id=&success=true');
+    }
+
+    else
+    {
+        // Mensagem de erro (vermelho)
+        echo '<div class="alert alert-danger mt-3 w-50">
+                <strong>Erro:</strong> Erro ao cadastrar serviço.
+              </div>';
+    }
+
+    echo '</div>';
+}
+
+>>>>>>> Stashed changes
 ?>
 
 <div class="container mt-5">
@@ -23,6 +54,7 @@ $Servico = new Servico;
             <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>
     </div>
+<<<<<<< Updated upstream
     
 <?php
 if(isset($_POST['nomeServico']))
@@ -56,3 +88,50 @@ if(isset($_POST['nomeServico']))
     echo '</div>';
 }
 ?>
+=======
+
+    <div class="container p-3">
+    <div class="card p-3 table-responsive">
+    <h2>Minha Lista de Serviços</h2>
+        <table class="table table-bordered table-hover">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Valor</th>
+                    <th>Descrição</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $result = $Servico->ConsultarServicosPrestador($decoded->id);
+
+
+                if ($result->num_rows > 0) {
+                    // Exibe os dados na tabela
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        echo "<td>" . $row['Id'] . "</td>";
+                        echo "<td>" . $row['Nome'] . "</td>";
+                        echo "<td>R$" . number_format($row['Valor'], 2, ',', '.') . "</td>";
+                        echo "<td>" . $row['Descricao'] . "</td>";
+                        echo "<td class='col-auto'>";
+                        echo "<a href='DetalhesServico.php?id=" . $row['Id'] . "' class='btn btn-sm btn-info'>Detalhes</a>";
+                        echo "<a href='page.php?arquivo=AlterarServico&id=" . $row['Id'] . "' class='btn btn-sm btn-warning'>Alterar</a>";
+                        echo "<a href='DeletarServico.php?id=" . $row['Id'] . "' class='btn btn-sm btn-danger'>Deletar</a>";
+                        echo "</td>";
+                        echo "</tr>";
+
+                    }
+                } else {
+                    echo "<tr><td colspan='3'>Nenhum serviço cadastrado.</td></tr>";
+                }
+
+                ?>
+            </tbody>
+        </table>
+
+        </div>
+        </div>
+>>>>>>> Stashed changes
